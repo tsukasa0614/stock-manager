ffd5f13768b8d59166c4a293fbf58a63ba63b581
miyamoto <miyamoto@DESKTOP-EH6LJN5>
2025-01-04 12:08:04 +0900

    初期化

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -15,6 +15,6 @@ const router = createBrowserRouter([
 ]);
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-      <RouterProvider router={router} />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
